85a9fc0a36b6e09cb86af6b89cda3425db0fdd3e
Jazmin M <jazminmerckel@gmail.com>
2019-11-06 23:06:29 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,7 +4,7 @@ require "pry"
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  binding.pry
+  #binding.pry
   # projects: kickstarter.css("li.project.grid_4")
 end
  
